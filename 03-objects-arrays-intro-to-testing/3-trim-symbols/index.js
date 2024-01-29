c2953bba830f274fa1a21aaf61254b8414a29552
axelx <cherniginas@edu.mos.ru>
2024-01-29 09:51:55 +0300

    правки: 3. / [trimSymbols]

--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -5,5 +5,22 @@
  * @returns {string} - the new string without extra symbols according passed size
  */
 export function trimSymbols(string, size) {
+  let res = ''
+  const obj = {
+    str: '',
+    cnt: 0,
+  }
+  for (let i = 0; i < string.length; i++) {
+    if (obj.str !== string.charAt(i)) {
+      obj.cnt = 0;
+    }
+    if (obj.cnt >= size){
+      continue;
+    }
+    obj.str = string.charAt(i);
+    obj.cnt += 1;
 
+    res += string.charAt(i);
+  }
+  return res;
 }
