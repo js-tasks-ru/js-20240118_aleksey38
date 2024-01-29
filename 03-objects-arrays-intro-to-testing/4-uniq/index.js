61b4e28bf850ad43ef375af623d921319d0da19b
axelx <cherniginas@edu.mos.ru>
2024-01-29 10:10:59 +0300

    решение: 3. / [uniq]

--- 03-objects-arrays-intro-to-testing/4-uniq/index.js
+++ 03-objects-arrays-intro-to-testing/4-uniq/index.js
@@ -4,5 +4,20 @@
  * @returns {*[]} - the new array with uniq values
  */
 export function uniq(arr) {
+  if (arr === undefined || arr.length === 0){
+    return []
+  }
 
+  const set = new Set()
+  const resArr = []
+
+  for (const key of arr) {
+    set.add(key)
+  }
+
+  for (const setKey of set) {
+    resArr.push(setKey)
+  }
+
+  return resArr
 }
