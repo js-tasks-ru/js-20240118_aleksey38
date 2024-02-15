97e9241a7b73bb390341bd491e91d41d5d10c7ec
axelx <cherniginas@edu.mos.ru>
2024-02-15 11:03:47 +0300

    6.1
    done
    :wq
    
    i
    :i

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -8,9 +8,9 @@ export function sortStrings(arr, param = 'asc') {
   const arrCopy = [...arr];
 
   if (param === 'asc') {
-    arrCopy.sort(compare_asc);
+    arrCopy.sort(compareAsc);
   } else if (param === 'desc') {
-    arrCopy.sort(compare_desc);
+    arrCopy.sort(compareDesc);
   }
   return arrCopy;
 }
@@ -19,8 +19,8 @@ const collator = new Intl.Collator(["ru-RU", "en-EN"], {
   caseFirst: "upper"
 });
 
-const compare_asc = (a, b) => collator.compare(a, b);
+const compareAsc = (a, b) => collator.compare(a, b);
 
-const compare_desc = (a, b) => {
+const compareDesc = (a, b) => {
   return collator.compare(a, b) * -1;
 };
